ea1015037ad374f67cbf16416588803be0a3ebed
Peter Xu <px.darkside@gmail.com>
2025-02-06 11:41:27 +0800

    refine cube2png

--- cube2png/cube2png.py
+++ cube2png/cube2png.py
@@ -1,12 +1,7 @@
 import argparse
 import os.path
+import sys
 import png
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
 
 def is_float(s):
     return s.replace('.', '', 1).isdigit()
@@ -23,27 +18,31 @@ def read_cube(fileName):
                 a = aLine.split()
                 local_lut_size = int(a[1])
                 local_lut_data = bytearray(local_lut_size * local_lut_size * local_lut_size * 4)
-                print(f"lut size = {local_lut_size}")
+                # print(f"lut size = {local_lut_size}")
                 continue
 
             if pixels < 0 and aLine.upper().startswith('#LUT DATA POINTS'):
                 pixels = 0
                 continue
 
-            if pixels < 0 and local_lut_size >= 0:
-                print(f"for line:{aLine}")
+            if pixels < 0 <= local_lut_size:
+                # print(f"for line:{aLine}")
                 tmp = aLine.split()
-                print(tmp)
+                # print(tmp)
                 if len(tmp) == 3 and is_float(tmp[0]) and is_float(tmp[1]) and is_float(tmp[2]):
                     pixels = 0
-                    print(f"set pixels = 0 by line: {aLine}")
+                    # print(f"set pixels = 0 by line: {aLine}")
                 else:
                     continue
 
             if pixels >= 0:
                 nums = aLine.split()
                 if len(nums) == 3:
-                    # print(nums)
+                    for n in nums:
+                        fn = float(n)
+                        if fn < 0.0 or fn > 1.0:
+                            raise ValueError(f'value must be in range(0,1.0), value = "{fn}"')
+
                     local_lut_data[pixels * 4] = int(float(nums[0]) * 255)
                     local_lut_data[pixels * 4 + 1] = int(float(nums[1]) * 255)
                     local_lut_data[pixels * 4 + 2] = int(float(nums[2]) * 255)
@@ -58,24 +57,44 @@ def write_png(fileName, lutSize, lutData):
         png_writer.write_array(aFile, lutData)
 
 
+def convert_cube(file_name):
+    output_file_name = file_name
+    if output_file_name.upper().endswith('.CUBE'):
+        output_file_name = output_file_name[:-len('.CUBE')]
+    output_file_name += '.png'
+    try:
+        local_lut_size, local_lut_data = read_cube(file_name)
+        if local_lut_size > 0 and len(local_lut_data) == local_lut_size * local_lut_size * local_lut_size * 4:
+            write_png(output_file_name, local_lut_size, local_lut_data)
+            print(f'{output_file_name} saved.')
+        else:
+            print(f'unrecognized input file "{file_name}"')
+    except ValueError as e:
+        print("\033[31m" + f'convert "{file_name}" failed: {e}' + "\033[0m")
+
+def convert_cube_in_dir(dir_name):
+    for name in os.listdir(dir_name):
+        full_path_name = os.path.join(dir_name, name)
+        if os.path.isfile(full_path_name) and full_path_name.upper().endswith('.CUBE'):
+            convert_cube(full_path_name)
+        elif os.path.isdir(full_path_name):
+            convert_cube_in_dir(full_path_name)
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("cubeFile", type=str, help="Input cube file")
     args = parser.parse_args()
 
-    if os.path.isfile(args.cubeFile) and os.path.exists(args.cubeFile):
-        outputFileName = str(args.cubeFile)
-        if outputFileName.upper().endswith('.CUBE'):
-            outputFileName = outputFileName[:-len('.CUBE')]
-        outputFileName += '.png'
+    if not os.path.exists(args.cubeFile):
+        print(f'Invalid input : "{args.cubeFile}"')
+        sys.exit()
 
-        lut_size, lut_data = read_cube(args.cubeFile)
-        if lut_size > 0 and len(lut_data) == lut_size * lut_size * lut_size * 4:
-            write_png(outputFileName, lut_size, lut_data)
-            print(f'{outputFileName} saved.')
-        else:
-            print(f'unrecognized input file "{args.cubeFile}"')
+    if os.path.isfile(args.cubeFile):
+        convert_cube(args.cubeFile)
+    elif os.path.isdir(args.cubeFile):
+        convert_cube_in_dir(args.cubeFile)
     else:
         print(f'Can\'t find input file "{args.cubeFile}"')
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
